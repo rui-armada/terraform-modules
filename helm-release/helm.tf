f27048be70d29e825f55200e2ca8ceb448939608
Rui Armada <rarmada@ext.worten.pt>
2023-04-01 01:05:49 +0100

    helm-release and kind cluster added

--- helm-release/helm.tf
+++ helm-release/helm.tf
@@ -17,12 +17,12 @@ resource "null_resource" "wait_for_ingress_nginx" {
   provisioner "local-exec" {
     command = <<EOF
       printf "\nWaiting for the nginx ingress controller...\n"
-      kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+      kubectl wait --namespace ${helm_release.example.namespace} \
         --for=condition=ready pod \
         --selector=app.kubernetes.io/component=controller \
         --timeout=90s
     EOF
   }
 
-  depends_on = [helm_release.ingress_nginx]
+  depends_on = [helm_release.example]
 }
